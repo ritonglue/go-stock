ea4dadb7ddbf131b5c843355acac8dccf8fbcc64
Frédéric Jean-Prost <jean-prost.frederic@neuf.fr>
2023-04-22 14:57:26 +0200

    remove import

--- src/main/java/io/github/ritonglue/gostock/StockManager.java
+++ src/main/java/io/github/ritonglue/gostock/StockManager.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
