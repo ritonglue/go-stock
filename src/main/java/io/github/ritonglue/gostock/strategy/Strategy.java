f151eda2296b11aaae8b04950afd431106186406
Frédéric Jean-Prost <jean-prost.frederic@neuf.fr>
2023-04-22 22:46:53 +0200

    add getStock

--- src/main/java/io/github/ritonglue/gostock/strategy/Strategy.java
+++ src/main/java/io/github/ritonglue/gostock/strategy/Strategy.java
@@ -3,6 +3,8 @@ package io.github.ritonglue.gostock.strategy;
 import java.math.BigDecimal;
 import java.util.Iterator;
 
+import javax.money.MonetaryAmount;
+
 import io.github.ritonglue.gostock.StockManager.Trade;
 
 public interface Strategy extends Iterable<Trade> {
@@ -13,17 +15,37 @@ public interface Strategy extends Iterable<Trade> {
 	void clear();
 	int size();
 
+	/**
+	 * @return the quantity in stock
+	 */
 	default BigDecimal getQuantity() {
 		Iterator<Trade> iterator = this.iterator();
-		BigDecimal stockQuantity = null;
+		BigDecimal quantity = BigDecimal.ZERO;
+		while(iterator.hasNext()) {
+			Trade tmp = iterator.next();
+			quantity = quantity.add(tmp.getQuantity());
+		}
+		return quantity;
+	}
+
+	/**
+	 * @return the quantity and buy value amount
+	 */
+	default Trade getStock() {
+		Iterator<Trade> iterator = this.iterator();
+		BigDecimal quantity = BigDecimal.ZERO;
+		//if stock is empty, amount is null : missing currency.
+		MonetaryAmount amount = null;
 		if(iterator.hasNext()) {
 			Trade tmp = iterator.next();
-			stockQuantity = tmp.getQuantity();
+			quantity = tmp.getQuantity();
+			amount = tmp.getAmount();
 		}
 		while(iterator.hasNext()) {
 			Trade tmp = iterator.next();
-			stockQuantity = stockQuantity.add(tmp.getQuantity());
+			quantity = quantity.add(tmp.getQuantity());
+			amount = amount.add(tmp.getAmount());
 		}
-		return stockQuantity;
+		return Trade.buy(quantity, amount, null);
 	}
 }
