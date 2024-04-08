07b19d58cc89f64526971566a33e0d0de7376461
Frédéric Jean-Prost <jean-prost.frederic@neuf.fr>
2024-04-08 21:18:11 +0200

    add test reduction

--- src/test/java/io/github/ritonglue/gostock/ModificationFIFOTest.java
+++ src/test/java/io/github/ritonglue/gostock/ModificationFIFOTest.java
@@ -83,4 +83,44 @@ public class ModificationFIFOTest {
 		Assert.assertEquals(createMoney("11.11"), modification.getAmountBefore());
 		Assert.assertEquals(createMoney("31.11"), modification.getAmountAfter());
 	}
+
+	@Test
+	public void testBuySellReduction() {
+		int id = 1;
+		List<TradeWrapper> list = new ArrayList<>();
+		SourceTest a = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(3), createMoney("33.33"), a));
+		SourceTest b = new SourceTest(id++);
+		list.add(TradeWrapper.sell(createQuantity(2), b));
+		SourceTest c = new SourceTest(id++);
+		list.add(TradeWrapper.modification(createMoney(-10), c));
+		StockManager manager = newStockManager();
+		manager.process(list);
+		TradeWrapper stock = manager.getStock();
+		Assert.assertEquals(createQuantity(1), stock.getQuantity());
+		Assert.assertEquals(createMoney("1.11"), stock.getAmount());
+
+		List<Position> openedPositions = manager.getOpenedPositions();
+		List<Position> closedPositions = manager.getClosedPositions();
+		List<Modification> modifications = manager.getModifications();
+		Assert.assertEquals(1, openedPositions.size());
+		Assert.assertEquals(1, closedPositions.size());
+		Assert.assertEquals(1, modifications.size());
+		Position position = openedPositions.get(0);
+		Assert.assertEquals(createQuantity(1), position.getQuantity());
+		Assert.assertEquals(a, position.getBuy());
+
+		position = closedPositions.get(0);
+		Assert.assertEquals(createQuantity(2), position.getQuantity());
+		Assert.assertEquals(createMoney("22.22"), position.getAmount());
+		Assert.assertEquals(a, position.getBuy());
+		Assert.assertEquals(b, position.getSell());
+
+		Modification modification = modifications.get(0);
+		Assert.assertEquals(createQuantity(1), modification.getQuantity());
+		Assert.assertEquals(createMoney(-10), modification.getModification().getAmount());
+		Assert.assertEquals(c, modification.getModification().getSource());
+		Assert.assertEquals(createMoney("11.11"), modification.getAmountBefore());
+		Assert.assertEquals(createMoney("1.11"), modification.getAmountAfter());
+	}
 }
