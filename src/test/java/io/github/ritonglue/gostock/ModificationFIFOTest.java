fb9a30cda29189abff3bc5e54fc81e91f611b363
Frédéric Jean-Prost <jean-prost.frederic@neuf.fr>
2024-05-01 10:52:51 +0200

    test reduction by quantity

--- src/test/java/io/github/ritonglue/gostock/ModificationFIFOTest.java
+++ src/test/java/io/github/ritonglue/gostock/ModificationFIFOTest.java
@@ -2,7 +2,11 @@ package io.github.ritonglue.gostock;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
@@ -42,6 +46,10 @@ public class ModificationFIFOTest {
 
 	private static StockManager newStockManager() {
 		return new StockManager(Mode.FIFO);
+	}
+
+	private static StockManager newStockManager(ModificationMode modificationMode) {
+		return new StockManager(Mode.FIFO, null, modificationMode);
 	}
 
 	@Test
@@ -122,5 +130,122 @@ public class ModificationFIFOTest {
 		Assert.assertEquals(c, modification.getModification().getSource());
 		Assert.assertEquals(createMoney("11.11"), modification.getAmountBefore());
 		Assert.assertEquals(createMoney("1.11"), modification.getAmountAfter());
+	}
+
+	@Test
+	public void testReductionByQuantityOnePositionToZero() {
+		int id = 1;
+		List<TradeWrapper> list = new ArrayList<>();
+		SourceTest a = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(3), createMoney("33.33"), a));
+		SourceTest b = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(4), createMoney("45.01"), b));
+		SourceTest c = new SourceTest(id++);
+		list.add(TradeWrapper.modification(createMoney(-78), c));
+		StockManager manager = newStockManager(ModificationMode.QUANTITY);
+		manager.process(list);
+		Map<Object, Position> openedPositions = manager.getOpenedPositions().stream().collect(Collectors.toMap(Position::getBuy, Function.identity()));
+		Assert.assertEquals(2, openedPositions.size());
+		Position position = openedPositions.get(a);
+		Assert.assertTrue(position.getAmount().isZero());
+		position = openedPositions.get(b);
+		Assert.assertEquals(createMoney("0.34"), position.getAmount());
+	}
+
+	@Test
+	public void testReductionByQuantityToZero() {
+		int id = 1;
+		List<TradeWrapper> list = new ArrayList<>();
+		SourceTest a = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(3), createMoney("33.33"), a));
+		SourceTest b = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(4), createMoney("45.01"), b));
+		SourceTest c = new SourceTest(id++);
+		list.add(TradeWrapper.modification(createMoney("-78.34"), c));
+		StockManager manager = newStockManager(ModificationMode.QUANTITY);
+		manager.process(list);
+		TradeWrapper stock = manager.getStock();
+		Assert.assertEquals(createMoney(0), stock.getAmount());
+		Assert.assertTrue(createQuantity(7).compareTo(stock.getQuantity()) == 0);
+		Map<Object, Position> openedPositions = manager.getOpenedPositions().stream().collect(Collectors.toMap(Position::getBuy, Function.identity()));
+		Assert.assertEquals(2, openedPositions.size());
+		Position position = openedPositions.get(a);
+		Assert.assertTrue(position.getAmount().isZero());
+		position = openedPositions.get(b);
+		Assert.assertTrue(position.getAmount().isZero());
+	}
+
+	@Test
+	public void testReductionByQuantity1() {
+		int id = 1;
+		List<TradeWrapper> list = new ArrayList<>();
+		SourceTest b = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(4), createMoney("45.01"), b));
+		SourceTest a = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(3), createMoney("33.33"), a));
+		SourceTest c = new SourceTest(id++);
+		list.add(TradeWrapper.modification(createMoney("-78.00"), c));
+		StockManager manager = newStockManager(ModificationMode.QUANTITY);
+		manager.process(list);
+		TradeWrapper stock = manager.getStock();
+		Assert.assertEquals(createMoney("0.34"), stock.getAmount());
+		Assert.assertTrue(createQuantity(7).compareTo(stock.getQuantity()) == 0);
+		Map<Object, Position> openedPositions = manager.getOpenedPositions().stream().collect(Collectors.toMap(Position::getBuy, Function.identity()));
+		Assert.assertEquals(2, openedPositions.size());
+		Position position = openedPositions.get(a);
+		Assert.assertTrue(position.getAmount().isZero());
+		position = openedPositions.get(b);
+		Assert.assertEquals(createMoney("0.34"), position.getAmount());
+	}
+
+	@Test
+	public void testReductionByQuantity2() {
+		int id = 1;
+		List<TradeWrapper> list = new ArrayList<>();
+		SourceTest a = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(3), createMoney("33.33"), a));
+		SourceTest b = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(4), createMoney("45.01"), b));
+		SourceTest c = new SourceTest(id++);
+		list.add(TradeWrapper.modification(createMoney("-78.00"), c));//there is a left over
+		StockManager manager = newStockManager(ModificationMode.QUANTITY);
+		manager.process(list);
+		TradeWrapper stock = manager.getStock();
+		Assert.assertEquals(createMoney("0.34"), stock.getAmount());
+		Assert.assertTrue(createQuantity(7).compareTo(stock.getQuantity()) == 0);
+		Map<Object, Position> openedPositions = manager.getOpenedPositions().stream().collect(Collectors.toMap(Position::getBuy, Function.identity()));
+		Assert.assertEquals(2, openedPositions.size());
+		Position position = openedPositions.get(a);
+		Assert.assertTrue(position.getAmount().isZero());
+		position = openedPositions.get(b);
+		Assert.assertEquals(createMoney("0.34"), position.getAmount());
+	}
+
+	@Test
+	public void testReductionByQuantity3() {
+		int id = 1;
+		List<TradeWrapper> list = new ArrayList<>();
+		SourceTest b = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(2), createMoney("1600"), b));
+		SourceTest c = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(3), createMoney("2700"), c));
+		SourceTest a = new SourceTest(id++);
+		list.add(TradeWrapper.buy(createQuantity(1), createMoney("100"), a));
+		SourceTest d = new SourceTest(id++);
+		//reduction by 200 per unit
+		list.add(TradeWrapper.modification(createMoney("-1100"), ModificationMode.QUANTITY, d));
+		StockManager manager = newStockManager();
+		manager.process(list);
+		TradeWrapper stock = manager.getStock();
+		Assert.assertEquals(createMoney("3300"), stock.getAmount());
+		Assert.assertTrue(createQuantity(6).compareTo(stock.getQuantity()) == 0);
+		Map<Object, Position> openedPositions = manager.getOpenedPositions().stream().collect(Collectors.toMap(Position::getBuy, Function.identity()));
+		Assert.assertEquals(3, openedPositions.size());
+		Position position = openedPositions.get(a);
+		Assert.assertTrue(position.getAmount().isZero());
+		position = openedPositions.get(b);
+		Assert.assertEquals(createMoney("1200"), position.getAmount());
+		position = openedPositions.get(c);
+		Assert.assertEquals(createMoney("2100"), position.getAmount());
 	}
 }
